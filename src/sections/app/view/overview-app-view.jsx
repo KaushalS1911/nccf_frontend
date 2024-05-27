b624cd78af60afb9d24f14b7d33d10cf2541855d
KaushalS19 <kaushalsojitra19@gmail.com>
2024-05-27 18:56:21 +0530

    - 27 th may (auth guard, favicon changes)

---
+++ src/sections/app/view/overview-app-view.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+
+import { useMockedUser } from 'src/hooks/use-mocked-user';
+
+import { SeoIllustration } from 'src/assets/illustrations';
+import { _appAuthors, _appRelated, _appFeatured, _appInvoices, _appInstalled } from 'src/_mock';
+
+import { useSettingsContext } from 'src/components/settings';
+
+import AppNewInvoice from '../app-new-invoice';
+import AppWidgetSummary from '../app-widget-summary';
+
+// ----------------------------------------------------------------------
+
+export default function OverviewAppView() {
+  const { user } = useMockedUser();
+
+  const theme = useTheme(); // Ensure useTheme hook is called here
+
+  const settings = useSettingsContext();
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'xl'}>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Active Users"
+            percent={2.6}
+            total={18765}
+            chart={{
+              series: [5, 18, 12, 51, 68, 11, 39, 37, 27, 20],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Installed"
+            percent={0.2}
+            total={4876}
+            chart={{
+              colors: [theme.palette.info.light, theme.palette.info.main], 
+              series: [20, 41, 63, 33, 28, 35, 50, 46, 11, 26],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={4}>
+          <AppWidgetSummary
+            title="Total Downloads"
+            percent={-0.1}
+            total={678}
+            chart={{
+              colors: [theme.palette.warning.light, theme.palette.warning.main],
+              series: [8, 9, 31, 8, 16, 37, 8, 33, 46, 31],
+            }}
+          />
+        </Grid>
+        <Grid xs={12} lg={12}>
+          <AppNewInvoice
+            title="New Invoice"
+            tableData={_appInvoices}
+            tableLabels={[
+              { id: 'id', label: 'Invoice ID' },
+              { id: 'category', label: 'Category' },
+              { id: 'price', label: 'Price' },
+              { id: 'status', label: 'Status' },
+              { id: '' },
+            ]}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
